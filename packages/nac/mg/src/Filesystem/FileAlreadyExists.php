29a2c795ba55206f4e41ef26973e5b340770925f
郑樟强 <nacrane2006@163.com>
2020-02-26 09:40:21 +0800

    composer require nac/mg

--- packages/nac/mg/src/Filesystem/FileAlreadyExists.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Nac\Mg\Filesystem;
-
-class FileAlreadyExists extends \Exception
-{
-}
--- packages/nac/mg/src/Filesystem/FileNotFound.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Nac\Mg\Filesystem;
-class FileNotFound extends \Exception
-{
-}
