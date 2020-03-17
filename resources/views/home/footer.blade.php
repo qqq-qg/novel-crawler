ab0a33ee52985cde611322273800a01b8c4cd789
zhengzhangqiang <nacrane2006@163.com>
2020-03-18 00:17:12 +0800

    123

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -1,12 +1,8 @@
 <div id="footer">
-    <p>
-        本站所有小说为转载作品，所有章节均由网友上传，转载至本站只是为了宣传本书让更多读者欣赏。
-    </p>
-    <p>
         {{--        {{ $SET['powerby']??'' }}--}}
         {{--<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");--}}
         {{--document.write(unescape("%3Cspan id='cnzz_stat_icon_1261657948'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1261657948%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>--}}
-    </p>
+    {{--</p>--}}
 </div>
 <script>
 function AddFavorite(c, a) {
